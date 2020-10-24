f9786f1dfc2207da02789726f4fcc3cdad0a394f
Huu Loc Nguyen <huu_loc.nguyen@share-now.com>
2020-10-24 13:27:46 +0200

    Travis build

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -2,11 +2,8 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 if [ $TRAVIS_BRANCH != "master" ]; then
-    docker tag locnh/k8s-puller locnh/k8s-puller:devel
-    docker push locnh/k8s-puller:devel
-elif [ -z $TRAVIS_TAG ]; then
-    docker tag locnh/k8s-puller locnh/k8s-puller:$TRAVIS_TAG
-    docker push locnh/k8s-puller:$TRAVIS_TAG
+    docker tag locnh/k8s-puller locnh/k8s-puller:$TRAVIS_BRANCH
+    docker push locnh/k8s-puller:$TRAVIS_BRANCH
 else
     docker push locnh/k8s-puller
 fi
